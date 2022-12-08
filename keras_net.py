505a9f6be6495b358be9c8c6079f48d7b5b9696e
Tezas-6174 <jamdade.2@iitj.ac.in>
2022-12-09 01:10:13 +0530

    train the models using the train_model function
    Signed-off-by: Tezas-6174 <jamdade.2@iitj.ac.in>

--- keras_net.py
+++ keras_net.py
@@ -43,65 +43,78 @@ def read_data(df):
     
     return np.array(features), np.array(labels)
 
-df = pd.read_csv('mixed_0101_abrupto.csv')
 
-x, y = read_data(df)
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+def train_model(x_train, x_test, y_train, y_test):
+
+    for i in range(exp_rounds):
+        print("***** Round %d *****" % i)
+        # keras neural net
+        tf.keras.backend.clear_session()
+        model = tf.keras.Sequential([
+            tf.keras.layers.Dense(
+                hidden_neuron_num, 
+                input_shape=(x_train.shape[1], ), 
+                activation='sigmoid', 
+                kernel_initializer='zeros',
+                # kernel_initializer=tf.keras.initializers.GlorotUniform(seed=42),
+                use_bias=False,
+                kernel_regularizer=tf.keras.regularizers.L2(l2_reg)),
+            # tf.keras.layers.Dense(hidden_neuron_num, activation='sigmoid'),
+            tf.keras.layers.Dense(
+                y_train.shape[1], 
+                activation='softmax', 
+                kernel_initializer='zeros',
+                # kernel_initializer=tf.keras.initializers.GlorotUniform(seed=42),
+                bias_initializer='zeros',
+                kernel_regularizer=tf.keras.regularizers.L2(l2_reg))
+        ])
+        opt = tf.keras.optimizers.SGD(learning_rate=l_r, momentum=0.0)
+        model.compile(optimizer=opt,
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+        
+        model.fit(x_train, y_train, batch_size=batch, epochs=epochs, verbose=0)
+
+        # stats
+        train_loss, train_accuracy = model.evaluate(x_train, y_train, verbose=0)
+        train_acc_keras.append(train_accuracy*100)
+        test_loss, test_accuracy = model.evaluate(x_test, y_test, verbose=0)
+        test_acc_keras.append(test_accuracy*100)
+
+        # results
+        print('Training accuracy:\n', train_accuracy*100)
+        print('Test accuracy:\n', test_accuracy*100)
+        print("Keras trained weights:\n", model.get_weights())
+
+        # sys.exit()
+        
+    print("\n"+"*"*20)
+    print("Hyper-parameters:")
+    print("learning rate: %.2f, l2 regularisor: %.2f, batch size: %d, epochs: %d" % (l_r, l2_reg, batch, epochs))
+    print("\n"+"*"*20)
+    print("%d Rounds Stats of Accuracy, Manual, Keras:" % exp_rounds)
+    print("Training Mean:", stat.mean(train_acc_keras))
+    print("Training Median:", stat.median(train_acc_keras))
+    print("Training Std Deviation:", stat.stdev(train_acc_keras))
+    print("Test Mean:", stat.mean(test_acc_keras))
+    print("Test Median:", stat.median(test_acc_keras))
+    print("Test Std Deviation:", stat.stdev(test_acc_keras))
+
+df_a = pd.read_csv('mixed_0101_abrupto.csv')
+df_b = pd.read_csv('mixed_1010_abrupto.csv')
+
+a_x, a_y = read_data(df_a)
+a_x_train, a_x_test, a_y_train, a_y_test = train_test_split(a_x, a_y, test_size=0.2, random_state=42)
+
+b_x, b_y = read_data(df_b)
+b_x_train, b_x_test, b_y_train, b_y_test = train_test_split(b_x, b_y, test_size=0.2, random_state=42)
+
+df_c = pd.merge(df_a, df_b) # Adding two datasets (dataset A + dataset B)
+
+c_x, c_y = read_data(df_c)
+c_x_train, c_x_test, c_y_train, c_y_test = train_test_split(c_x, c_y, test_size=0.2, random_state=42)
 
 # # test
 # print('y_train:\n', y_train)
-
-
-
-for i in range(exp_rounds):
-    print("***** Round %d *****" % i)
-    # keras neural net
-    tf.keras.backend.clear_session()
-    model = tf.keras.Sequential([
-        tf.keras.layers.Dense(
-            hidden_neuron_num, 
-            input_shape=(x_train.shape[1], ), 
-            activation='sigmoid', 
-            kernel_initializer='zeros',
-            # kernel_initializer=tf.keras.initializers.GlorotUniform(seed=42),
-            use_bias=False,
-            kernel_regularizer=tf.keras.regularizers.L2(l2_reg)),
-        # tf.keras.layers.Dense(hidden_neuron_num, activation='sigmoid'),
-        tf.keras.layers.Dense(
-            y_train.shape[1], 
-            activation='softmax', 
-            kernel_initializer='zeros',
-            # kernel_initializer=tf.keras.initializers.GlorotUniform(seed=42),
-            bias_initializer='zeros',
-            kernel_regularizer=tf.keras.regularizers.L2(l2_reg))
-    ])
-    opt = tf.keras.optimizers.SGD(learning_rate=l_r, momentum=0.0)
-    model.compile(optimizer=opt,
-                loss='categorical_crossentropy',
-                metrics=['accuracy'])
-    
-    model.fit(x_train, y_train, batch_size=batch, epochs=epochs, verbose=0)
-
-    # stats
-    train_loss, train_accuracy = model.evaluate(x_train, y_train, verbose=0)
-    train_acc_keras.append(train_accuracy*100)
-    test_loss, test_accuracy = model.evaluate(x_test, y_test, verbose=0)
-    test_acc_keras.append(test_accuracy*100)
-
-    # results
-    print('Training accuracy:\n', train_accuracy*100)
-    print('Test accuracy:\n', test_accuracy*100)
-    print("Keras trained weights:\n", model.get_weights())
-
-# sys.exit()
-print("\n"+"*"*20)
-print("Hyper-parameters:")
-print("learning rate: %.2f, l2 regularisor: %.2f, batch size: %d, epochs: %d" % (l_r, l2_reg, batch, epochs))
-print("\n"+"*"*20)
-print("%d Rounds Stats of Accuracy, Manual, Keras:" % exp_rounds)
-print("Training Mean:", stat.mean(train_acc_keras))
-print("Training Median:", stat.median(train_acc_keras))
-print("Training Std Deviation:", stat.stdev(train_acc_keras))
-print("Test Mean:", stat.mean(test_acc_keras))
-print("Test Median:", stat.median(test_acc_keras))
-print("Test Std Deviation:", stat.stdev(test_acc_keras))
+train_model(a_x_train, b_x_test, a_y_train, b_y_test)
+train_model(c_x_train, b_x_test, c_y_train, b_y_test)

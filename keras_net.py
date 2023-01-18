01d9df9ce86cf3a5f36a755010697fed76f924af
suluz <zhigang.lu@outlook.com>
2023-01-18 16:42:59 +1100

    modify reading data function a bit

--- keras_net.py
+++ keras_net.py
@@ -6,14 +6,15 @@ import statistics as stat
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
+import sys
 
 # hyper-parameters
 batch = 10 # batch size
 epochs = 100 # training epoch number
-hidden_neuron_num = [1] # number of neurons in each hidden layer, you can add as many hidden layers as possible
+hidden_neuron_num = [10,10] # number of neurons in each hidden layer, you can add as many hidden layers as possible
 l_r = 0.1 # learning rate
 l2_reg = 0.01 # l2 regulariser
-exp_rounds = 10 # number of runs to produce average results
+exp_rounds = 1 # number of runs to produce average results
 train_acc_man = []
 test_acc_man = []
 timing_man = []
@@ -23,25 +24,26 @@ timing_keras = []
 
 # read data
 # fill the read_data() func yourself
-def read_data(df):
+def read_data(df, label_col_index):
     # write you data reading function here
     # features should be min-max normalised
     # labels should be converted to one-hot encoded list
     # returned values are two numpy arrays
 
     # initialising the features and labels as the part of dataframes (series)
-    df_features = df.iloc[:,:4]
-    arr = df.iloc[:,4].to_numpy()
+    arr = df.iloc[:,label_col_index].to_numpy()
     df_labels = arr.reshape(-1,1)
+    features = df.drop([label_col_index], axis=1).to_numpy()
     
     # initialising the scaler (min-max) and encoder (one-hot)
     scaler = MinMaxScaler()
     encoder = OneHotEncoder(sparse=False)
 
-    features = scaler.fit_transform(df_features)    
+    feats = scaler.fit_transform(features)    
     labels = encoder.fit_transform(df_labels)
     
-    return np.array(features), np.array(labels)
+    # return np.array(features), np.array(labels)
+    return feats, labels
 
 
 def train_model(x_train, x_test, y_train, y_test):
@@ -116,18 +118,27 @@ def train_model(x_train, x_test, y_train, y_test):
     print("Test Median:", stat.median(test_acc_keras))
     print("Test Std Deviation:", stat.stdev(test_acc_keras))
 
+# # test - read iris data
+# df = pd.read_csv('iris.data', header=None)
+# x, y = read_data(df, 4)
+# print(x[0][0])
+# print(y[0])
+
+# sys.exit()
+
+
 df_a = pd.read_csv('mixed_0101_abrupto.csv')
 df_b = pd.read_csv('mixed_1010_abrupto.csv')
 
-a_x, a_y = read_data(df_a)
+a_x, a_y = read_data(df_a, 4)
 a_x_train, a_x_test, a_y_train, a_y_test = train_test_split(a_x, a_y, test_size=0.2, random_state=42)
 
-b_x, b_y = read_data(df_b)
+b_x, b_y = read_data(df_b, 4)
 b_x_train, b_x_test, b_y_train, b_y_test = train_test_split(b_x, b_y, test_size=0.2, random_state=42)
 
 df_c = pd.merge(df_a, df_b) # Adding two datasets (dataset A + dataset B)
 
-c_x, c_y = read_data(df_c)
+c_x, c_y = read_data(df_c, 4)
 c_x_train, c_x_test, c_y_train, c_y_test = train_test_split(c_x, c_y, test_size=0.2, random_state=42)
 
 # # test
